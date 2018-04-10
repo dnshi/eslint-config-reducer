a99a77670a5d29a778c7d85f3cd1a0145a7c0c02
Dean Shi <dean.xiaoshi@gmail.com>
2018-04-10 10:59:25 -0700

    Improve code structure for react

--- react.js
+++ react.js
@@ -1,5 +1,9 @@
 const base = require('./index.js')
 
-module.exports = Object.assign({}, base, {
+module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
-})
+  plugins: ['prettier'],
+  rules: Object.assign({}, base.rules, {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  }),
+}
