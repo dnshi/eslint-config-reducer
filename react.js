b5eae60b00b3730470648e2362dfd7ecacefe294
Dean Shi <dnshi@users.noreply.github.com>
2018-04-20 10:23:51 -0700

    Update react.js

--- react.js
+++ react.js
@@ -3,6 +3,9 @@ const base = require('./index.js')
 module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
   plugins: ['prettier'],
+  env: {
+    browser: true,
+  },
   rules: Object.assign({}, base.rules, {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   }),
