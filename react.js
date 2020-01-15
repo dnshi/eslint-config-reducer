6debab911b6b6889a3b2d0e7c4d1865905fc509d
Dean Shi <dean.xiaoshi@gmail.com>
2020-01-15 15:04:10 -0800

    Fix resolving issue

--- react.js
+++ react.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: ['./index', 'airbnb', 'airbnb/hooks', 'prettier/react'],
+  extends: ['./index', 'airbnb', 'airbnb/hooks', 'prettier/react'].map(
+    require.resolve,
+  ),
   env: {
     browser: true,
   },
--- typescript.js
+++ typescript.js
@@ -4,8 +4,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
     'prettier/@typescript-eslint',
-  ],
+  ].map(require.resolve),
   rules: {
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx', '.jsx'],
+      },
+    ],
   },
 }
